8c27c8b942b224c6996877cbb35adf6fa8bc4869
larenelg <larene@dccjobs.com>
2018-10-23 14:45:17 +1000

    add good boy

--- React/src/components/GoodBoyForm.js
+++ React/src/components/GoodBoyForm.js
@@ -1,8 +1,30 @@
 import React, { Component } from 'react';
+import goodBoyPicture from './good-boy.jpg';
 
 class GoodBoyForm extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            goodBoy: false
+        }
+
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick() {
+        if (!this.state.goodBoy)
+        {
+            this.setState({ goodBoy: true })
+        }
+    }
+
     render() {
-        return <h1>On a scale of one to 1,000 how good boy are you?</h1>;
+        if (this.state.goodBoy)
+        {
+            return <img src={goodBoyPicture}></img>
+        }
+        return <button onClick={this.handleClick}>Click for good boy</button>;
     }
 }
 
--- React/webpack.config.js
+++ React/webpack.config.js
@@ -13,7 +13,7 @@ module.exports = {
                 loader: 'file-loader',
                 options: {
                     name: '[name].[hash].[ext]',
-                    publicPath: '/components/'
+                    publicPath: '/React/dist/'
                 },
             },
             {
